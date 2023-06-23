0a16ad542349a7727e3a0d360ac287abcc617fcf
judedaniel2023 <daniel.jude.87536@unn.edu.ng>
2023-06-23 03:51:27 +0100

    completed category and blog model

---
+++ blogs/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Category, Blog
+
+
+# Register your models here.
+
+class BlogModel(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'category', 'author','status', 'is_featured')
+    search_fields  = ('title', 'category', 'author','status')
+    list_editable = ('is_featured',)
+
+
+admin.site.register(Category)
+admin.site.register(Blog, BlogModel)
+
